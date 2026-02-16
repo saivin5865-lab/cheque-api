75ec739f38fe010b2cb7cef631bae20302d8aa91
sai <sadasaiteja45@gmail.com>
2026-02-16 16:39:22 +0530

    Initial cheque detection API

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, File, UploadFile
+from ultralytics import YOLO
+import shutil
+import uuid
+import os
+
+app = FastAPI(title="Cheque Detection API")
+
+model = YOLO("best.pt")
+
+@app.get("/")
+def home():
+    return {"message": "Cheque Detection API Running"}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+
+    temp_filename = f"{uuid.uuid4()}.jpg"
+
+    with open(temp_filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    results = model.predict(temp_filename, conf=0.25)
+
+    detections = []
+
+    for box in results[0].boxes:
+        cls_id = int(box.cls)
+        conf = float(box.conf)
+        class_name = model.names[cls_id]
+        coords = box.xyxy.tolist()[0]
+
+        detections.append({
+            "field": class_name,
+            "confidence": round(conf, 3),
+            "bbox": coords
+        })
+
+    os.remove(temp_filename)
+
+    return {
+        "status": "success",
+        "detections": detections
+    }
